7be7b15e3b9b7265a1e33a333f1d70e454ec7510
KiriqN <jared@inboundmarketing.co.za>
2026-09-16 17:26:22 +0200

    Add initial project setup with Tailwind CSS integration
    - Created package.json for project metadata and dependencies
    - Added input.css to import Tailwind CSS
    - Generated output.css with Tailwind CSS styles

--- scripts/tipCalc.js
+++ scripts/tipCalc.js
@@ -1,1 +1,8 @@
 `use strict`;
+
+const button = addEventListener("testButton", testFunction);
+
+function testFunction() {
+  document.getElementById("testButton");
+  console.log("hello world");
+}
