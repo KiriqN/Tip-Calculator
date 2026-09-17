f6f5f05c547a71eb1e8ab6f0d5ca0f1f81573e1b
KiriqN <jared@inboundmarketing.co.za>
2026-09-17 10:04:35 +0200

    Refactor form structure and enhance JavaScript functionality for tip calculation

--- scripts/tipCalc.js
+++ scripts/tipCalc.js
@@ -1,8 +1,26 @@
-`use strict`;
-
-const button = addEventListener("testButton", testFunction);
+"use strict";
 
 function testFunction() {
-  document.getElementById("testButton");
-  console.log("hello world");
+  const tipDetails = [];
+
+  const waiterList = document.getElementById("waiterList").value;
+  const tableNo = document.getElementById("tableNo").value;
+  const amount = document.getElementById("amount").value;
+  const currency = document.getElementById("currency").value;
+
+  tipDetails.push(waiterList, tableNo, amount, currency);
+
+  console.log(tipDetails);
+  //splitArray(tipDetails);
 }
+
+/*(function splitArray(details) {
+  tipDetails.forEach((item) => {
+    if (typeof item === "number") {
+      amountDetails.push(item);
+      console.log(amountDetails);
+    }
+  });
+
+  return details;
+} */
